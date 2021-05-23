7e09b26db94f6efe09f6fe1062a530bf034f699d
ilhamexalt <wmeluck24@gmail.com>
2021-05-23 22:33:56 +0700

    menambahkan sweetalert pada pesan

--- assets/js/main.js
+++ assets/js/main.js
@@ -27,7 +27,7 @@ navLink.forEach(n => n.addEventListener('click', linkAction));
 
 /*===== SCROLL REVEAL ANIMATION =====*/
 const sr = ScrollReveal({
-    origin: 'top',
+    origin: 'left',
     distance: '80px',
     duration: 2000,
     reset: true
@@ -35,7 +35,7 @@ const sr = ScrollReveal({
 
 /*SCROLL HOME*/
 sr.reveal('.home__title', {});
-sr.reveal('.button', { delay: 200 });
+sr.reveal('.button', { delay: 500 });
 sr.reveal('.home__img', { delay: 400 });
 sr.reveal('.home__social-icon', { interval: 200 });
 
